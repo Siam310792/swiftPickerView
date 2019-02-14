d00b283e0c9292d04c06b1bc31f98dd85d9f7a2f
Annabelle <annabelle.braye@gmail.com>
2019-02-14 11:24:58 +0100

    Ajout clouer à la seconde méthode

--- pickerColor/ColorPickerViewController.swift
+++ pickerColor/ColorPickerViewController.swift
@@ -10,36 +10,18 @@ import UIKit
 
 class ColorPickerViewController: UIViewController {
 
-    var delegate : ColorPickerViewDelegate?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-     /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    var completionHandler: ((UIColor) -> ())?
     
     @IBAction func greenButton(_ sender: Any) {
-        delegate?.userDidChooseColor(color: ColorAsset.green.color)
+        completionHandler?(ColorAsset.green.color)
     }
     
     @IBAction func orangeButton(_ sender: Any) {
-        delegate?.userDidChooseColor(color: ColorAsset.orange.color)
+        completionHandler?(ColorAsset.orange.color)
     }
     
     @IBAction func purpleButton(_ sender: Any) {
-        delegate?.userDidChooseColor(color: ColorAsset.purple.color)
+        completionHandler?(ColorAsset.purple.color)
     }
 }
 
-protocol ColorPickerViewDelegate {
-    func userDidChooseColor(color: UIColor)
-}
--- pickerColor/ViewController.swift
+++ pickerColor/ViewController.swift
@@ -18,14 +18,18 @@ class ViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "pickColor") {
             let destVC = segue.destination as! ColorPickerViewController
-            destVC.delegate = self
+            destVC.completionHandler = { color in
+                self.view.backgroundColor = color
+                self.dismiss(animated: true, completion: nil)
+                
+            }
         }
     }
     
 
 }
 
-extension ViewController:ColorPickerViewDelegate {
+extension ViewController {
     func userDidChooseColor(color: UIColor) {
         self.view.backgroundColor = color
         self.dismiss(animated: true, completion: nil)
